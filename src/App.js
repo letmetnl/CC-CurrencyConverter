29511d8ae365d6e5e26651e1e15598714e3a389f
mohits Projects <mohit5150515@gmail.com>
2021-03-04 17:51:47 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import CurrencyRow from "./CurrencyRow";
+
+const API_URL = "https://api.exchangeratesapi.io/latest";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lets Convert</h1>
+      <CurrencyRow />
+      <div className="equals">=</div>
+      <CurrencyRow />
     </div>
   );
 }
