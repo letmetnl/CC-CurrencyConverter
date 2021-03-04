266fc938b27ecfa19ff290358a2099c1d4b9127f
mohits Projects <mohit5150515@gmail.com>
2021-03-04 21:11:23 +0530

    setcurrency and options

--- src/App.js
+++ src/App.js
@@ -6,23 +6,34 @@ const API_URL = "https://api.exchangeratesapi.io/latest";
 
 function App() {
   const [currencyOptions, setCurrencyOptions] = useState([]);
-  console.log(currencyOptions);
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
+  // console.log(currencyOptions);
   // will useEffect which takes function and array as params wherein any changes in elements of array cause rerendering
   useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
+        const firstCurrency = Object.keys(data.rates)[0];
         // console.log(data);
         setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+        setFromCurrency(data.base);
+        setToCurrency(firstCurrency);
       });
   }, []); // [] means we only wanna call this for once at app loading
 
   return (
     <div className="App">
       <h1>Lets Convert</h1>
-      <CurrencyRow />
+      <CurrencyRow
+        currencyOptions={currencyOptions} // passing as props
+        preSelectedCurrency={fromCurrency}
+      />
       <div className="equals">=</div>
-      <CurrencyRow />
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        preSelectedCurrency={toCurrency}
+      />
     </div>
   );
 }
